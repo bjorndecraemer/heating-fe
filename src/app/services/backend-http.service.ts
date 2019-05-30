2233159b096a4257d118d9d70a36ca6b5b0716df
Bjorn De Craemer <bjorn.decraemer@yahoo.com>
2019-05-30 22:54:03 +0200

    Intermediate

--- src/app/services/backend-http.service.ts
+++ src/app/services/backend-http.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HeatingService} from "./heating.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class BackendHttpService {
 
-  constructor() { }
+  private heaterSubject;
+  private pumpSubject;
+
+  constructor(private heatingService : HeatingService) { }
+
+  public activateBurner(){
+    this.heatingService
+  }
 }
