76e57d651063a4f097db9242ae7e5fa71bc20219
Bjorn DC <bjorn.decraemer@yahoo.com>
2019-12-11 02:16:58 +0100

    Added humidity and temperature

--- src/app/services/backend-http.service.ts
+++ src/app/services/backend-http.service.ts
@@ -7,41 +7,52 @@ import {Observable} from 'rxjs';
 })
 export class BackendHttpService {
 
-  private baseUrl: string = "http://192.168.1.30:8080/api/fecontrol/";
-  private pumpEndpoint: string = "pump";
-  private burnerEndpoint: string = "burner";
-  private heatingEndpoint: string = "heating";
+  private baseUrl = 'http://192.168.1.30:8080/api/fecontrol/';
+  // private baseUrl: string = "http://localhost:8080/api/fecontrol/"
+  private pumpEndpoint = 'pump';
+  private burnerEndpoint = 'burner';
+  private heatingEndpoint = 'heating';
+  private humidityEndpoint = 'humidity';
+  private tempEndpoint = 'temp';
 
   constructor(private http: HttpClient) { }
 
-  public activateBurner() : Observable<Object>{
-    return this.http.put(this.baseUrl+this.burnerEndpoint+"?setting=true","")
+  public activateBurner(): Observable<Object> {
+    return this.http.put(this.baseUrl + this.burnerEndpoint + '?setting=true', '');
   }
 
-  public deactivateBurner() : Observable<Object>{
-    return this.http.put(this.baseUrl+this.burnerEndpoint+"?setting=false","")
+  public deactivateBurner(): Observable<Object> {
+    return this.http.put(this.baseUrl + this.burnerEndpoint + '?setting=false', '');
   }
 
-  public activateHeating() : Observable<Object>{
-    return this.http.put(this.baseUrl+this.heatingEndpoint+"?setting=true","")
+  public activateHeating(): Observable<Object> {
+    return this.http.put(this.baseUrl + this.heatingEndpoint + '?setting=true', '');
   }
 
-  public deactivateHeating() : Observable<Object>{
-    return this.http.put(this.baseUrl+this.heatingEndpoint+"?setting=false","")
+  public deactivateHeating(): Observable<Object> {
+    return this.http.put(this.baseUrl + this.heatingEndpoint + '?setting=false', '');
   }
 
-  public activatePump() : Observable<Object>{
-    return this.http.put(this.baseUrl+this.pumpEndpoint+"?setting=true","")
+  public activatePump(): Observable<Object> {
+    return this.http.put(this.baseUrl + this.pumpEndpoint + '?setting=true', '');
   }
 
-  public deactivatePump() : Observable<Object>{
-    return this.http.put(this.baseUrl+this.pumpEndpoint+"?setting=false","")
+  public deactivatePump(): Observable<Object> {
+    return this.http.put(this.baseUrl + this.pumpEndpoint + '?setting=false', '');
   }
 
-  public getPumpStatus() : Observable<Object>{
-    return this.http.get(this.baseUrl+this.pumpEndpoint);
+  public getPumpStatus(): Observable<Object> {
+    return this.http.get(this.baseUrl + this.pumpEndpoint);
   }
-  public getBurnerStatus() : Observable<Object>{
-    return this.http.get(this.baseUrl+this.burnerEndpoint);
+  public getBurnerStatus(): Observable<Object> {
+    return this.http.get(this.baseUrl + this.burnerEndpoint);
+  }
+
+  public getTemperature(): Observable<Object> {
+    return this.http.get(this.baseUrl + this.tempEndpoint);
+  }
+
+  public getHumidity(): Observable<Object> {
+    return this.http.get(this.baseUrl + this.humidityEndpoint);
   }
 }
--- src/app/services/heating.service.ts
+++ src/app/services/heating.service.ts
@@ -8,14 +8,18 @@ import {flatMap, map} from 'rxjs/operators';
 })
 export class HeatingService {
 
-  public burnerSubject : Subject<Boolean> = new Subject();
-  public pumpSubject : Subject<Boolean> = new Subject();
-  public loadingSubject : Subject<Boolean> = new Subject();
+  public burnerSubject: Subject<Boolean> = new Subject();
+  public pumpSubject: Subject<Boolean> = new Subject();
+  public loadingSubject: Subject<Boolean> = new Subject();
+  public tempSubject: Subject<string> = new Subject<string>();
+  public humiditySubject: Subject<string> = new Subject<string>();
 
   constructor(private backendHttpService: BackendHttpService) {
     this.loadingSubject.next(false);
     this.getPumpStatus();
     this.getBurnerStatus();
+    this.getTempStatus();
+    this.getHumidityStatus();
   }
 
   public activateBurner() {
@@ -37,17 +41,30 @@ export class HeatingService {
     this.backendHttpService.deactivatePump().subscribe(() => this.pumpSubject.next(false));
   }
 
-  public triggerStatusUpdate(){
+  public triggerStatusUpdate() {
     this.loadingSubject.next(true);
-    this.getPumpStatus().pipe(flatMap(()=> this.getBurnerStatus())).subscribe(()=> this.loadingSubject.next(false));
-    //forkJoin(this.getPumpStatus(), this.getBurnerStatus, () => {}).subscribe(()=> this.loadingSubject.next(false));
+    this.getPumpStatus().pipe(
+        flatMap(() => this.getBurnerStatus()),
+        flatMap(() => this.getTempStatus()),
+        flatMap(() => this.getHumidityStatus())
+    )
+        .subscribe(() => this.loadingSubject.next(false));
+    // forkJoin(this.getPumpStatus(), this.getBurnerStatus, () => {}).subscribe(()=> this.loadingSubject.next(false));
   }
 
-  private getPumpStatus() : Observable<void>{
-    return this.backendHttpService.getPumpStatus().pipe(map((res : boolean) => this.pumpSubject.next(res)));
+  private getPumpStatus(): Observable<void> {
+    return this.backendHttpService.getPumpStatus().pipe(map((res: boolean) => this.pumpSubject.next(res)));
   }
 
-  private getBurnerStatus() : Observable<void>{
-    return this.backendHttpService.getBurnerStatus().pipe(map((res : boolean) => this.burnerSubject.next(res)));
+  private getBurnerStatus(): Observable<void> {
+    return this.backendHttpService.getBurnerStatus().pipe(map((res: boolean) => this.burnerSubject.next(res)));
+  }
+
+  private getTempStatus(): Observable<void> {
+    return this.backendHttpService.getTemperature().pipe(map((res: string) => this.tempSubject.next(res)));
+  }
+
+  private getHumidityStatus(): Observable<void> {
+    return this.backendHttpService.getHumidity().pipe(map((res: string) => this.humiditySubject.next(res)));
   }
 }
